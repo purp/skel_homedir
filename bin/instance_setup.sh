2059fe2f01d6413f43f6716967e2d0e20213bc31
Jim Meyer <jim@geekdaily.org>
2016-09-13 16:59:56 -0700

    Bugfixes

--- bin/instance_setup.sh
+++ bin/instance_setup.sh
@@ -5,10 +5,11 @@
 # Handy bits of info
 export INSTANCE_ID=`curl --silent --connect-timeout 2 http://169.254.169.254/latest/meta-data/instance-id`
 export INSTANCE_REGION=`curl --silent --connect-timeout 1 http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's/[a-z]$//'`
-export VOLUME_ID=`aws ec2 describe-volumes --filters Name=tag-value,Values="work" --query 'Volumes[*].{ID:VolumeId}' --output text`
 
 # Push the region into the config
 aws configure set region $INSTANCE_REGION
+
+export VOLUME_ID=`aws ec2 describe-volumes --filters Name=tag-value,Values="work" --query 'Volumes[*].{ID:VolumeId}' --output text`
 
 # Attach work volume
 aws ec2 attach-volume --volume-id $VOLUME_ID --instance-id $INSTANCE_ID --device /dev/sdb
